5083cb7f54d1a01955203a499d67c26e7d463ff6
AntonP22 <91823229+AntonP22@users.noreply.github.com>
2023-04-27 20:31:14 +0300

    27.04.2023

--- src/components/Messages.jsx
+++ src/components/Messages.jsx
@@ -1,3 +1,16 @@
+import textImg from "../assets/img/Left side.jpg";
+
 export const Messages = () => {
-    return <h1>Это страница с сообщениями</h1>;
+    return (
+      <>
+        <div className="row">
+          <div className="col-md-6">
+            <img src={textImg} alt="img-fluid" />
+          </div>
+          <div className="col-md-6">Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius, molestias, aliquam, repellat fugiat quia ducimus mollitia laboriosam reprehenderit corporis a laudantium odit eligendi reiciendis voluptatum possimus porro totam id sapiente?
+          Debitis necessitatibus nisi eligendi, similique optio ab sequi aut. Corrupti, molestiae sit unde ab soluta corporis facilis est voluptatem vero labore rem suscipit, id expedita commodi nesciunt doloremque. Rem, accusantium?
+          Dicta animi maxime mollitia dolores beatae ipsum quo maiores eos voluptatibus quaerat cumque, nihil necessitatibus nemo magni laborum deserunt excepturi neque ea enim eligendi nisi saepe ab dolorem? Fugiat, quo?</div>
+        </div>
+      </>
+    )
   };
