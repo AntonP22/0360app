404bf0ce198b6a2d1d84b7ed5a162fee457e3bca
AntonP22 <91823229+AntonP22@users.noreply.github.com>
2023-04-27 20:31:45 +0300

    27.03.2023

--- src/components/Messages.jsx
+++ src/components/Messages.jsx
@@ -1,4 +1,5 @@
 import textImg from "../assets/img/Left side.jpg";
+import backgroundImg from "../assets/img/sea.jpeg";
 
 export const Messages = () => {
     return (
@@ -10,6 +11,13 @@ export const Messages = () => {
           <div className="col-md-6">Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius, molestias, aliquam, repellat fugiat quia ducimus mollitia laboriosam reprehenderit corporis a laudantium odit eligendi reiciendis voluptatum possimus porro totam id sapiente?
           Debitis necessitatibus nisi eligendi, similique optio ab sequi aut. Corrupti, molestiae sit unde ab soluta corporis facilis est voluptatem vero labore rem suscipit, id expedita commodi nesciunt doloremque. Rem, accusantium?
           Dicta animi maxime mollitia dolores beatae ipsum quo maiores eos voluptatibus quaerat cumque, nihil necessitatibus nemo magni laborum deserunt excepturi neque ea enim eligendi nisi saepe ab dolorem? Fugiat, quo?</div>
+        </div>
+        <div className="row">
+          <div className="col-xl-6 offset-6 text-color-bg" style={{backgroundImage: `url(${backgroundImg})`}}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae magnam officiis similique veritatis ducimus eligendi. Hic qui veniam nobis magnam, praesentium obcaecati. Laborum incidunt dolorem tenetur repudiandae omnis libero veniam.
+            Voluptatibus, sint, provident suscipit soluta cupiditate eius nulla pariatur ipsa corporis reprehenderit, obcaecati assumenda nihil facere enim. Fuga ut aperiam sapiente, impedit sequi commodi expedita mollitia temporibus provident ducimus voluptatum.
+            Quos iusto vero perferendis recusandae accusantium delectus rem nostrum earum nobis est aperiam numquam laboriosam iste sequi nesciunt, suscipit nisi eligendi reiciendis. Beatae laborum unde ut. Reprehenderit nisi rem sint.
+          </div>
         </div>
       </>
     )
